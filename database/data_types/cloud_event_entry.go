6d4294567e1c5770088955ef70b14e55ad422bed
Ivan Mascarenhas <ivanmascarenhas@kofre.com.br>
2019-05-15 15:40:19 -0300

    commit inicial

--- database/data_types/cloud_event_entry.go
+++ database/data_types/cloud_event_entry.go
@@ -21,7 +21,6 @@ type CloudEvent struct {
 // default spec version from 0.2 to the provided version.
 func NewCloudEventV02(eventType string, data string, extensions map[string]interface{}) (*CloudEvent, error) {
 	e := &CloudEvent{}
-	e.Context.AsV02()
 	err := e.Context.SetSpecVersion(cloudevents.CloudEventsVersionV02)
 	if err != nil {
 		return nil, err
