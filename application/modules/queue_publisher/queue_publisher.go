ea496b492e646be7daa4a0e11e2e16c328c563ac
Ivan Mascarenhas <ivanmascarenhas@kofre.com.br>
2019-06-24 23:55:33 -0300

    commit inicial

--- application/modules/queue_publisher/queue_publisher.go
+++ application/modules/queue_publisher/queue_publisher.go
@@ -1,1 +1,12 @@
 package queue_publisher
+
+import "github.com/kofre/lib-queue/rabbitmq_repository"
+
+type queue_publisher struct {
+	queueRepo rabbitmq_repository.QueueRepository
+	queueMap  map[string]rabbitmq_repository.Queue
+}
+
+func InitQueuePublisher() {
+
+}
--- application/modules/timer_control/timer_control.go
+++ application/modules/timer_control/timer_control.go
@@ -2,6 +2,7 @@ package timer_control
 
 import (
 	"context"
+	"devgit.kf.com.br/comercial/gateway-maxtrack/module_maxtrack/logger"
 	"fmt"
 	"github.com/ivanmeca/timedEvent/application/modules/database/collection_managment"
 	"github.com/ivanmeca/timedEvent/application/modules/database/data_types"
@@ -13,6 +14,7 @@ type TimerControl struct {
 	expirationTime time.Duration
 	controlTime    time.Duration
 	list           *sync.Map
+	logger         *logger.StdLogger
 }
 
 func NewTimerControl(controlTime int, expirationTime int, list *sync.Map) *TimerControl {
@@ -30,6 +32,7 @@ func (tc *TimerControl) Run(ctx context.Context) {
 			}
 		}
 	}()
+	tc.logger = logger.GetLogger()
 }
 
 func (tc *TimerControl) processList() {
@@ -39,18 +42,27 @@ func (tc *TimerControl) processList() {
 		if event, ok := value.(data_types.EventMapper); ok {
 			timeDiffInSecond := horaAtual.Sub(event.PublishDate)
 			timeDiffInSecond /= time.Second
-			fmt.Printf("Hora atual: %s, hora do evento: %s\n", horaAtual.Format("2006-01-02 15:04:05Z"), event.PublishDate.Format("2006-01-02 15:04:05Z"))
+			tc.logger.DebugPrintln("Hora atual: %s, hora do evento: %s\n", horaAtual.Format("2006-01-02 15:04:05Z"), event.PublishDate.Format("2006-01-02 15:04:05Z"))
 			if timeDiffInSecond > tc.expirationTime {
 				_, err := collection_managment.NewEventCollection().DeleteItem([]string{event.EventID})
 				if err != nil {
-					fmt.Printf("falha ao excluir ID: " + event.EventID)
+					tc.logger.NoticePrintln("falha ao excluir ID: " + event.EventID)
 				}
 				tc.list.Delete(key)
-				fmt.Println("ID excluido: " + event.EventID)
+				tc.logger.DebugPrintln("ID excluido: " + event.EventID)
 			} else {
 				if timeDiffInSecond > 0 {
-					fmt.Println("Publicar ID" + event.EventID)
-					//todo: publicar evento
+					tc.logger.DebugPrintln("Publicar ID" + event.EventID)
+					data, err := collection_managment.NewEventCollection().ReadItem(event.EventID)
+					if err != nil {
+						tc.logger.ErrorPrintln("event check fail: " + err.Error())
+						return true
+					}
+					if data.ArangoRev == event.EventRevision {
+						//todo: publicar evento
+					} else {
+						tc.list.Delete(key)
+					}
 				}
 			}
 		}
