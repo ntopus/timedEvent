5d593287b0efe106cf77ac8f0736ce808547b1fb
Ivan Mascarenhas <ivanmascarenhas@kofre.com.br>
2019-06-25 01:33:56 -0300

    commit inicial

--- application/modules/timer_control/timer_control.go
+++ application/modules/timer_control/timer_control.go
@@ -59,7 +59,7 @@ func (tc *TimerControl) processList() {
 						return true
 					}
 					if data.ArangoRev == event.EventRevision {
-						if queue_publisher.QueuePublisher().PublishInQueue(data.PublishQueue, event.Event) {
+						if queue_publisher.QueuePublisher().PublishInQueue(data.PublishQueue, event.Event.CloudEvent) {
 							_, err := collection_managment.NewEventCollection().DeleteItem([]string{event.EventID})
 							if err != nil {
 								tc.logger.NoticePrintln("falha ao excluir ID: " + event.EventID)
