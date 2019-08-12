b3d6be790262840b4d68547a4272f935608d4cdd
Ivan Mascarenhas <ivanmascarenhas@kofre.com.br>
2019-08-12 16:49:35 -0300

    Test refactoring

--- tests/webserver.go
+++ tests/webserver.go
@@ -5,6 +5,7 @@ import (
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -22,20 +23,55 @@ const (
 
 func CreateEventRequest() {
 	ginkgo.It("Valid msg", func() {
-		for i := 1; i < 300; i++ {
+		for i := 1; i < 15000; i++ {
 			strIvalue := strconv.Itoa(i)
-			fmt.Println("Trying to create an event " + strIvalue)
+			//fmt.Println("Trying to create an event " + strIvalue)
 			mockReader, err := GetMockReader(getMockEvent(time.Now(), strIvalue))
 			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 			h := make(map[string]string)
 			h[CONTENT_TYPE] = CONTENT_TYPE_CE
-			h[PUBLISH_DATE] = time.Now().Add(time.Duration(i) * 100 * time.Millisecond).UTC().Format(DATE_FORMAT)
+			h[PUBLISH_DATE] = time.Now().Add(time.Duration(i) * time.Millisecond).UTC().Format(DATE_FORMAT)
 			h[PUBLISH_QUEUE] = TEST_PUBLISH_QUEUE
 			h[PUBLISH_TYPE] = TEST_PUBLISH_TYPE
-			resp, err := SendPostRequestWithHeaders(TEST_ENDPOINT, mockReader, h)
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			gomega.Expect(resp.StatusCode).To(gomega.Equal(201))
+			go func() {
+				resp, err := SendPostRequestWithHeaders(TEST_ENDPOINT, mockReader, h)
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+				gomega.Expect(resp.StatusCode).To(gomega.Equal(201))
+			}()
 		}
+		mu := sync.Mutex{}
+		q := GetQueue(TEST_PUBLISH_QUEUE, 5)
+		mu.Lock()
+		count := 0
+		mu.Unlock()
+		q.OnPublishedEvent = func(message interface{}) {
+			mu.Lock()
+			defer mu.Unlock()
+			count++
+			fmt.Println("Publish OK")
+		}
+		mu.Lock()
+		countErr := 0
+		mu.Unlock()
+		q.OnNotPublishedEvent = func(message interface{}) {
+			mu.Lock()
+			defer mu.Unlock()
+			countErr++
+			fmt.Println("Publish Err")
+		}
+		err := q.StartConsume(func(queueName string, msg []byte) bool {
+			mu.Lock()
+			defer mu.Unlock()
+			count++
+			fmt.Println("New message")
+			return true
+		})
+		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+		gomega.Eventually(func() int {
+			mu.Lock()
+			defer mu.Unlock()
+			return count
+		}).Should(gomega.BeEquivalentTo(1))
 	})
 }
 
