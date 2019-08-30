2d2f2c13b3a2a4f01f46d9b041e67d034dbfaa0d
Ivan Mascarenhas <ivanmascarenhas@kofre.com.br>
2019-08-30 16:55:43 -0300

    feature-BS-20
    Benchmark testing

--- application/modules/config/config_schema.go
+++ application/modules/config/config_schema.go
@@ -1,30 +1,30 @@
 package config
 
 type ConfigData struct {
-	LogLevel       int
-	ServerPort     int
-	PoolTime       int
-	ControlTime    int
-	ExpirationTime int
-	DataBase       ConfigDB
-	PublishQueue   []ConfigQueue
+	LogLevel       int           `json:"logLevel"`
+	ServerPort     int           `json:"serverPort"`
+	PoolTime       int           `json:"poolTime"`
+	ControlTime    int           `json:"controlTime"`
+	ExpirationTime int           `json:"expirationTime"`
+	DataBase       ConfigDB      `json:"dataBase"`
+	PublishQueue   []ConfigQueue `json:"loglevel"`
 }
 
 type ConfigQueue struct {
-	ServerHost     string
-	ServerVHost    string
-	ServerPort     string
-	ServerUser     string
-	ServerPassword string
-	QueueName      string
+	ServerHost     string `json:"serverHost"`
+	ServerVHost    string `json:"serverVHost"`
+	ServerPort     string `json:"serverPort"`
+	ServerUser     string `json:"serverUser"`
+	ServerPassword string `json:"serverPassword"`
+	QueueName      string `json:"queueName"`
 }
 
 type ConfigDB struct {
-	ServerHost     string
-	ServerPort     string
-	ServerUser     string
-	ServerPassword string
-	DbName         string
+	ServerHost     string `json:"serverHost"`
+	ServerPort     string `json:"serverPort"`
+	ServerUser     string `json:"serverUser"`
+	ServerPassword string `json:"serverPassword"`
+	DbName         string `json:"dbName"`
 }
 
 func (c *ConfigDB) GetServerHost() string {
